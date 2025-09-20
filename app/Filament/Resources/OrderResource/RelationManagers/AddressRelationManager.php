1efd1e42eb5bc4753bba2308cb718025853e8902
faridanaksoleh <itspuyd@gmail.com>
2025-09-20 21:34:23 +0700

    first commit e-commerce project

---
+++ app/Filament/Resources/OrderResource/RelationManagers/AddressRelationManager.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AddressRelationManager extends RelationManager
+{
+    protected static string $relationship = 'address';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                TextInput::make('first_name')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('last_name')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('phone')
+                ->required()
+                ->tel()
+                ->maxLength(255),
+
+                TextInput::make('city')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('state')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('zip_code')
+                ->required()
+                ->numeric()
+                ->maxLength(255),
+
+                Textarea::make('street_address')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('street_address')
+            ->columns([
+                TextColumn::make('full_name')
+                ->label('Full Name'),
+
+                TextColumn::make('phone')
+                ->label('Phone'),
+
+                TextColumn::make('city')
+                ->label('City'),
+
+                TextColumn::make('state')
+                ->label('State'),
+
+                TextColumn::make('zip_code')
+                ->label('Zip Code'),
+
+                TextColumn::make('street_address')
+                ->label('Street Address'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="text-3xl text-red-500">Hello livewire 3</h1>
+</div>
