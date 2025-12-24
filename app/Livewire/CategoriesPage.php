f12c32be90c86102dba41d5c95bbf830a1cf6ee3
faridanaksoleh <itspuyd@gmail.com>
2025-12-24 07:26:29 +0700

    feat(categories): buat jadi dinamis

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Category; // <--- TAMBAHKAN BARIS INI
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Categories - DCodeMania')]
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+        // Sekarang PHP tahu kalau Category itu ada di App\Models
+        $categories = Category::where('is_active', 1)->get();
+        
+        return view('livewire.categories-page', [
+            'categories' => $categories,
+        ]);
     }
 }
